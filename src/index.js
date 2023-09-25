0167ab0fc1b4a3b7fb91f345f6cc047c27ecc290
Teplovozovich <artur.dmitriev.2016@inbox.ru>
2023-09-25 16:26:41 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    let number = '';
+    for (let i = 0; i < String(n).length; i++) {
+        if (String(n)[i] === "-") {
+            result = "" + result;
+        } else {
+            result = String(n)[i] + result;
+        }
+
+    }
+    return result;
+
 }
